e21bfad86324f4191fef8d0c43e712efae05d0e2
KeithCVMS <116287511+KeithCVMS@users.noreply.github.com>
2024-05-15 11:31:27 -0600

    Update RemoveBloat.ps1

--- De-Bloat/RemoveBloat.ps1
+++ De-Bloat/RemoveBloat.ps1
@@ -845,25 +845,25 @@ If ($null -ne $WinPackage)
 } 
 
 ##Tweak reg permissions
-#invoke-webrequest -uri "https://github.com/andrew-s-taylor/public/raw/main/De-Bloat/SetACL.exe" -outfile "C:\Windows\Temp\SetACL.exe"
-#C:\Windows\Temp\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Communications" -ot reg -actn setowner -ownr "n:$everyone"
-# C:\Windows\Temp\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Communications" -ot reg -actn ace -ace "n:$everyone;p:full"
+invoke-webrequest -uri "https://github.com/andrew-s-taylor/public/raw/main/De-Bloat/SetACL.exe" -outfile "C:\Windows\Temp\SetACL.exe"
+C:\Windows\Temp\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Communications" -ot reg -actn setowner -ownr "n:$everyone"
+ C:\Windows\Temp\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Communications" -ot reg -actn ace -ace "n:$everyone;p:full"
 
 #
-###Stop it coming back
-#$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Communications"
-#If (!(Test-Path $registryPath)) { 
-#    New-Item $registryPath
-#}
+##Stop it coming back
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Communications"
+If (!(Test-Path $registryPath)) { 
+    New-Item $registryPath
+}
 #Set-ItemProperty $registryPath ConfigureChatAutoInstall -Value 0
 
 
 ##Unpin it
-#$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Chat"
-#If (!(Test-Path $registryPath)) { 
-#    New-Item $registryPath
-#}
-#Set-ItemProperty $registryPath "ChatIcon" -Value 2
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Chat"
+If (!(Test-Path $registryPath)) { 
+    New-Item $registryPath
+}
+Set-ItemProperty $registryPath "ChatIcon" -Value 2
 write-host "Removed Teams Chat"
 ############################################################################################################
 #                                           Windows Backup App                                             #
