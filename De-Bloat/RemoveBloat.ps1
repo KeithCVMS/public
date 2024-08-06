8c75a10246f1f13de1dcb11800719302852aed48
KeithCVMS <116287511+KeithCVMS@users.noreply.github.com>
2024-08-06 15:12:46 -0600

    Add files via upload

--- De-Bloat/RemoveBloat.ps1
+++ De-Bloat/RemoveBloat.ps1
@@ -2142,8 +2142,7 @@ write-host "safeconnall:"$Safeconnects
 ForEach ($sc in $safeconnects) {
     If ($sc.UninstallString) {
 write-host "safecon:"$sc.uninstallstring
-$sc.Uninstallstring + " -s"
-cmd.exe /c $sc.UninstallString /quiet /norestart
+cmd.exe /c $sc.UninstallString /quiet /norestart -s
     }
 }
 ##
