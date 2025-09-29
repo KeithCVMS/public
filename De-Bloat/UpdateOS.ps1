57b180c9817ecda5e31bd9f25c402c72bef92462
KeithCVMS <116287511+KeithCVMS@users.noreply.github.com>
2025-09-29 11:16:35 -0600

    Add files via upload

--- De-Bloat/UpdateOS.ps1
+++ De-Bloat/UpdateOS.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.10
+.VERSION 2.1
 
 .GUID 07e4ef9f-8341-4dc4-bc73-fc277eb6b4e6
 
@@ -25,7 +25,8 @@
 .EXTERNALSCRIPTDEPENDENCIES 
 
 .RELEASENOTES
-Version CVMMPA: change tag and log directories to CVMMPA
+Version 2.1:  Added -Append for Start-Transcript.  Added logic to filter out feature updates.
+Version 2.0:  Restructured download and install logic
 Version 1.10: Fixed AcceptEula logic.
 Version 1.9:  Added -ExcludeUpdates switch.
 Version 1.8:  Added logic to pass the -ExcludeDrivers switch when relaunching as 64-bit.
@@ -37,11 +38,13 @@ Version 1.3:  Force use of Microsoft Update/WU.
 Version 1.2:  Updated to work on ARM64.
 Version 1.1:  Cleaned up output.
 Version 1.0:  Original published version.
-KH changes	inclusion of NETFX3 install here rather than in AutopilotBranding
+
+KH changes	added Log function
+			inclusion of NETFX3 install here rather than in AutopilotBranding
+			change of paths for CVMMPA locations
 			part of cahnge to call updateos from intune platform script rather than win32
    		if (!$_.EulaAccepted) { $_.AcceptEula() } patch courtesy mdmplayground
-   		
-			
+
 #>
 
 <#
@@ -53,7 +56,6 @@ This script uses the Windows Update COM objects to install the latest cumulative
 .\UpdateOS.ps1
 #>
 
-
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory = $False)] [ValidateSet('Soft', 'Hard', 'None', 'Delayed')] [String] $Reboot = 'Soft',
@@ -62,20 +64,23 @@ Param(
     [Parameter(Mandatory = $False)] [switch] $ExcludeUpdates
 )
 
-#Log function added KH
-Function Log() {
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory=$false)] [String] $message
-	)
 
-	$tz = [Regex]::Replace([System.TimeZoneInfo]::Local.StandardName, '([A-Z])\w+\s*', '$1')
-	$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-	Write-Output "$ts $tz -  $message"
-}
 
 Process {
 
+	#Log function added KH
+	Function Log() {
+		[CmdletBinding()]
+		param (
+			[Parameter(Mandatory=$false)] [String] $message
+		)
+
+		$tz = [Regex]::Replace([System.TimeZoneInfo]::Local.StandardName, '([A-Z])\w+\s*', '$1')
+		$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+		Write-Output "$ts $tz -  $message"
+	}
+	#End Log Function KH
+	
     # If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
     if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64") {
         if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe") {
@@ -91,36 +96,35 @@ Process {
     }
 
     # Create a tag file just so Intune knows this was installed
-    if (-not (Test-Path "$($env:ProgramData)\CVMMPA")) {
-        Mkdir "$($env:ProgramData)\CVMMPA"
+    if (-not (Test-Path "$($env:ProgramData)\CVMMPA")) {	#KH
+        Mkdir "$($env:ProgramData)\CVMMPA"					#KH
     }
-    Set-Content -Path "$($env:ProgramData)\CVMMPA\UpdateOS.tag" -Value "Start Script $(get-date)"
+    Set-Content -Path "$($env:ProgramData)\CVMMPA\UpdateOS.tag" -Value "Start Script $(get-date)"	#KH
 
     # Start logging
-    Start-Transcript "$($env:ProgramData)\CVMMPA\UpdateOS.log"
+    Start-Transcript "$($env:ProgramData)\CVMMPA\UpdateOS.log"	#KH
 
     # Main logic
     $script:needReboot = $false
 
-	$ci = Get-Computerinfo
-	Log "OSversion:$($ci.OsName)"
-	Log "OSBuild:$($ci.OsBuildNumber)"
-	Log " "
+    $ci = Get-Computerinfo				#KH
+	Log "OSversion:$($ci.OsName)"		#KH
+	Log "OSBuild:$($ci.OsBuildNumber)"	#KH
+	Write-Host " "					#KH
 
-#Now install OS Updates
-    # Opt into Microsoft Update
-    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-    Write-Output "$ts Opting into Microsoft Update"
+	#Now install OS Updates
+	# Opt into Microsoft Update
+#    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+    Log "Opting into Microsoft Update"
     $ServiceManager = New-Object -ComObject "Microsoft.Update.ServiceManager"
     $ServiceID = "7971f918-a847-4430-9279-4a52d1efe18d"
     $ServiceManager.AddService2($ServiceId, 7, "") | Out-Null
 
     # Install all available updates
-    $WUDownloader = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateDownloader()
+    Log "Install OS Update"
+	$WUDownloader = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateDownloader()
     $WUInstaller = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateInstaller()
- 
- 
-	if ($ExcludeDrivers) {
+    if ($ExcludeDrivers) {
         # Updates only
         $queries = @("IsInstalled=0 and Type='Software'")
     }
@@ -132,100 +136,95 @@ Process {
         $queries = @("IsInstalled=0 and Type='Software'", "IsInstalled=0 and Type='Driver'")
     }
 
+    $WUUpdates = New-Object -ComObject Microsoft.Update.UpdateColl
     $queries | ForEach-Object {
-
-        $WUUpdates = New-Object -ComObject Microsoft.Update.UpdateColl
-		write-host " " 
-		$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-        Log "$ts Getting $_ updates"        
-<#
-       ((New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search($_)).Updates | ForEach-Object {
-            if (!$_.EulaAccepted) { $_.AcceptEula() }
-            if ($_.Title -notmatch "Preview") { [void]$WUUpdates.Add($_) }
-        }
-#>
-		$CandidateUpdates = ((New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search($_)).Updates 
-		ForEach ($CandidateUpdate in $CandidateUpdates) {
-			If ($CandidateUpdate.Title -notmatch "Preview") {
-				If ($CandidateUpdate | Get-Member EulaAccepted) {
-					if (!$CandidateUpdate.EulaAccepted) {
-						$CandidateUpdate.AcceptEula() 
-					} 
-				}
-				ForEach ($CandidateUpdateCategory in $CandidateUpdate.Categories) {
-					if ($CandidateUpdateCategory.CategoryID -ne "3689BDC8-B205-4AF4-8D4A-A63924C5E9D5" -and $CandidateUpdate -notin $WUUpdates) {
-						Log "  Title: $($CandidateUpdate.Title)"
-						[void]$WUUpdates.Add($CandidateUpdate)
-					}
-				}
-			}
-		}
-
-		write-host " "
-		Log "UpdatesToDo: $($WUUpdates.Count)"
-		if ($WUUpdates.Count -ge 1) {
-            $WUInstaller.ForceQuiet = $true
-            $WUInstaller.Updates = $WUUpdates
-            $WUDownloader.Updates = $WUUpdates
-            $UpdateCount = $WUDownloader.Updates.count
-            if ($UpdateCount -ge 1) {
-                $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-                Write-Output " $ts Downloading $UpdateCount Updates"
-#                foreach ($update in $WUDownloader.Updates) { Write-Output "  Dwnld:$($update.Title)" }
-                $Download = $WUDownloader.Download()
+#        $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+        Log "Getting $_ updates."        
+        try {
+            ((New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search($_)).Updates | ForEach-Object {
+                if (!$_.EulaAccepted) { $_.AcceptEula() }
+                $featureUpdate = $_.Categories | Where-Object { $_.CategoryID -eq "3689BDC8-B205-4AF4-8D4A-A63924C5E9D5" }
+                if ($featureUpdate) {
+#                    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+                    Log "Skipping feature update: $($_.Title)"
+                } elseif ($_.Title -match "Preview") { 
+#                    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+                    Log "Skipping preview update: $($_.Title)"
+                } else {
+                    [void]$WUUpdates.Add($_)
+                }
             }
-            $InstallUpdateCount = $WUInstaller.Updates.count
-            if ($InstallUpdateCount -ge 1) {
-                $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-                Write-Output "$ts Installing $InstallUpdateCount Updates"
-#                foreach ($update in $WUInstaller.Updates) { Write-Output "  Inst:$($update.Title)" }
-				$Install = $WUInstaller.Install()
-                $ResultMeaning = ($Results | Where-Object { $_.ResultCode -eq $Install.ResultCode }).Meaning
-                Write-Output $ResultMeaning
-                $script:needReboot = $Install.RebootRequired
-			}
+        } catch {
+            # If this script is running during specialize, error 8024004A will happen:
+            # 8024004A	Windows Update agent operations are not available while OS setup is running.
+            $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+            Write-Warning "$ts Unable to search for updates: $_"
         }
-        else {
-            Write-Output "No Updates Found"
-        } 
+    }
+
+#    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+    if ($WUUpdates.Count -eq 0) {
+       Log "No Updates Found"
+        Exit 0
+    } else {
+        Log "Updates found: $($WUUpdates.count)"
+    }
+    
+    foreach ($update in $WUUpdates) {
+    
+        $singleUpdate = New-Object -ComObject Microsoft.Update.UpdateColl
+        $singleUpdate.Add($update) | Out-Null
+    
+        $WUDownloader = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateDownloader()
+        $WUDownloader.Updates = $singleUpdate
+    
+        $WUInstaller = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateInstaller()
+        $WUInstaller.Updates = $singleUpdate
+        $WUInstaller.ForceQuiet = $true
+    
+#        $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+        Log "Downloading update: $($update.Title)"
+        $Download = $WUDownloader.Download()
+        $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+        Log "Download result: $($Download.ResultCode) ($($Download.HResult))"
+    
+#        $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+        Log "Installing update: $($update.Title)"
+        $Results = $WUInstaller.Install()
+#        $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+        Log "Install result: $($Results.ResultCode) ($($Results.HResult))"
+
+        # result code 2 = success, see https://learn.microsoft.com/en-us/windows/win32/api/wuapi/ne-wuapi-operationresultcode
+
+        if ($Results.RebootRequired) {
+            $script:needReboot = $true
+        }
     }
 
     # Specify return code
 #    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
     if ($script:needReboot) {
-        Log "Windows Update indicated that a reboot is needed."
+        Log " Windows Update indicated that a reboot is needed."
+
+        $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+        if ($Reboot -eq "Hard") {
+            Log " Exiting with return code 1641 to indicate a hard reboot is needed."
+            Stop-Transcript
+            Exit 1641
+        }
+        elseif ($Reboot -eq "Soft") {
+            Log " Exiting with return code 3010 to indicate a soft reboot is needed."
+            Stop-Transcript
+            Exit 3010
+        }
+        elseif ($Reboot -eq "Delayed") {
+            Log " Rebooting with a $RebootTimeout second delay"
+            & shutdown.exe /r /t $RebootTimeout /c "Rebooting to complete the installation of Windows updates."
+            Exit 0
+        }    
     }
     else {
-        Log "Windows Update indicated that no reboot is required."
+        Log " Windows Update indicated that no reboot is required."
     }
-
-    # For whatever reason, the reboot needed flag is not always being properly set.  So we always want to force a reboot.
-    # If this script (as an app) is being used as a dependent app, then a hard reboot is needed to get the "main" app to
-    # install.
-#    $ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-
- if ($Reboot -eq "Hard") {
-        Log "Exiting with return code 1641 to indicate a hard reboot is needed."
-        Stop-Transcript
-        Exit 1641
-    }
-    elseif ($Reboot -eq "Soft") {
-        Log "Exiting with return code 3010 to indicate a soft reboot is needed."
-        Stop-Transcript
-        Exit 3010
-    }
-    elseif ($Reboot -eq "Delayed") {
-        Log "Rebooting with a $RebootTimeout second delay"
-        & shutdown.exe /r /t $RebootTimeout /c "Rebooting to complete the installation of Windows updates."
-        Stop-Transcript
-        Exit 0
-    }
-    else {
-        Log "Kkipping reboot based on Reboot parameter (None)"
-        Stop-Transcript
-        Exit 0
-    }
-
-Stop-Transcript
-
+    Exit 0
 }
