f274969f7ee4705bd6d0d0e99b402b0b114dc715
KeithCVMS <116287511+KeithCVMS@users.noreply.github.com>
2025-10-15 09:19:35 -0600

    Add files via upload

--- De-Bloat/SetTimeZone.ps1
+++ De-Bloat/SetTimeZone.ps1
@@ -12,37 +12,14 @@ Function Log() {
 #Log function end
 
 	
-#Create CVMMPA Customization Folder
-$AppFolder = "C:\ProgramData\CVMMPA"
-If (!(Test-Path $AppFolder)) {
-    New-Item -Path "$AppFolder" -ItemType Directory
-    Log "The folder $AppFolder was successfully created."
-}
-<# $AppLog = "$AppFolder\SetTimeZone-$(Get-Date -UFormat "%Y-%m-%d_%H-%M").log"
-
-Start-Transcript $AppLog
- #>
-$AppTag = "$AppFolder\SetTimeZone.tag"
-$UsrNm = $Env:UserName
-$CurrProf = $Env:Userprofile
-
-If (Test-Path $AppTag) {
-	Add-Content -Path "$AppTag" -Value "Start Script- $(get-date) - $CurrProf - $UsrNm"
-}
-Else {
-	Set-Content -Path "$AppTag" -Value "Start Script $(get-date) - $CurrProf - $UsrNm"
-}
-
-$CVMtz = "Mountain Standard Time"
+ $CVMtz = "Mountain Standard Time"
 
 If ($CVMtz -ieq (Get-TimeZone).Id) {
 	Log "TimeZone currently set correctly:"
-	Get-Date
 	Get-TimeZone
 }
 Else { 
 	Log "Current TimeZone: "
-	Get-Date
 	Get-TimeZone
 
 	Log "set TimeZone to: $CVMtz"
@@ -50,7 +27,6 @@ Else {
 	Set-TimeZone -Id $CVMtz -PassThru
 
 	Log "New TimeZone: "
-	Get-Date
 	Get-TimeZone
 }
 
